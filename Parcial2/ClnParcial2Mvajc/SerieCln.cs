26f0e5f5dda8f02bbcb35e4c6a00769af454555c
marieljanco <jancoa795@gmail.com>
2024-11-04 13:36:44 -0400

    bd, cln

---
+++ Parcial2/ClnParcial2Mvajc/SerieCln.cs
@@ -0,0 +1,80 @@
+﻿using CadParcial2Mvajc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClnParcial2Mvajc
+{
+    public class SerieCln
+    {
+
+		public static int insertar(Serie serie)
+		{
+			using (var context = new Parcial2MvajcEntities())
+			{
+				context.Serie.Add(serie);
+				context.SaveChanges();
+				return serie.id;
+			}
+		}
+
+		public static int actualizar(Serie serie)
+		{
+			using (var context = new Parcial2MvajcEntities())
+			{
+				var existente = context.Serie.Find(serie.id);
+				if (existente != null)
+				{
+					existente.titulo = serie.titulo;
+					existente.sinopsis = serie.sinopsis;
+					existente.director = serie.director;
+					existente.episodios = serie.episodios;
+					existente.fechaEstreno = serie.fechaEstreno;
+					existente.estado = serie.estado;
+					return context.SaveChanges();
+				}
+				return 0;
+			}
+		}
+
+		public static int eliminar(int id, string usuario)
+		{
+			using (var context = new Parcial2MvajcEntities())
+			{
+				var serie = context.Serie.Find(id);
+				if (serie != null)
+				{
+					serie.estado = -1;
+					return context.SaveChanges();
+				}
+				return 0;
+			}
+		}
+
+		public static Serie obtenerUno(int id)
+		{
+			using (var context = new Parcial2MvajcEntities())
+			{
+				return context.Serie.Find(id);
+			}
+		}
+
+		public static List<Serie> listar()
+		{
+			using (var context = new Parcial2MvajcEntities())
+			{
+				return context.Serie.Where(x => x.estado != -1).ToList();
+			}
+		}
+
+		public static List<paSerieListar_Result> listarPa(string parametro)
+		{
+			using (var context = new Parcial2MvajcEntities())
+			{
+				return context.paSerieListar(parametro).ToList();
+			}
+		}
+	}
+}
---
+++ Parcial2/ddl_dml_parcial2.sql
@@ -0,0 +1,50 @@
+﻿CREATE DATABASE Parcial2Mvajc;
+GO
+
+USE Parcial2Mvajc;
+GO
+
+USE [master];
+GO
+CREATE LOGIN [usrparcial2] WITH PASSWORD=N'12345678',
+    DEFAULT_DATABASE = Parcial2Mvajc,
+    CHECK_EXPIRATION = OFF,
+    CHECK_POLICY = ON;
+GO
+USE Parcial2Mvajc;
+GO
+CREATE USER [usrparcial2] FOR LOGIN [usrparcial2];
+GO
+ALTER ROLE [db_owner] ADD MEMBER [usrparcial2];
+GO
+
+CREATE TABLE Serie (
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    titulo VARCHAR(250),
+    sinopsis VARCHAR(5000),
+    director VARCHAR(100),
+    episodios INT,
+    fechaEstreno DATE
+);
+GO
+
+ALTER TABLE Serie ADD estado SMALLINT NOT NULL DEFAULT 1; -- -1: Eliminado, 0: Inactivo, 1: Activo
+GO
+
+CREATE PROCEDURE paSerieListar 
+    @parametro VARCHAR(50)
+AS
+BEGIN
+    SELECT id, titulo, sinopsis, director, episodios, fechaEstreno
+    FROM Serie
+    WHERE estado <> -1 AND titulo LIKE '%' + REPLACE(@parametro, ' ', '%') + '%';
+END
+GO
+
+INSERT INTO Serie (titulo, sinopsis, director, episodios, fechaEstreno)
+VALUES
+('Breaking Bad', 'Un profesor de química se convierte en fabricante de metanfetaminas.', 'Vince Gilligan', 62, '2008-01-20'),
+('The Walking Dead', 'Un grupo de sobrevivientes lucha por vivir en un mundo post-apocalíptico infestado de zombis.', 'Frank Darabont', 177, '2010-10-31'),
+('Okupas', 'La vida de un grupo de jóvenes que ocupa una casa en Buenos Aires y sus desafíos diarios.', 'Jorge Nisco', 11, '2000-08-30');
+
+SELECT * FROM Serie;
